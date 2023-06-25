af0e906529306170ce7b33cead90a194b49aa33a
Ayrat <DIMANVAZ04@GMAIL.COM>
2023-06-25 21:22:24 +0300

    div into 2 routes

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-#CMD [ "python", "./main.py" ]
-#CMD [ "python", "./example.py" ]
 CMD [ "python", "./fonetika.py" ]
--- fonetika.py
+++ fonetika.py
@@ -7,8 +7,38 @@ from fonetika.soundex import RussianSoundex
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def process_words():
+
+@app.route('/only_boolean', methods=['POST'])
+def only_boolean():
+    data = request.get_json()
+
+    given_word = data.get('given')
+    answer_word = data.get('answer')
+    limit = data.get('limit', 2)
+
+    soundex = RussianSoundex(delete_first_letter=True)
+    phon_distance = PhoneticsInnerLanguageDistance(soundex)
+    distance = phon_distance.distance(given_word, answer_word)
+    match = False
+
+    if distance <= limit:
+        match = True
+
+    response_data = {
+        'match': match
+    }
+
+    response = app.response_class(
+        response=json.dumps(response_data, sort_keys=False),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+
+@app.route('/full_info', methods=['POST'])
+def full_info():
     data = request.get_json()
 
     given_word = data.get('given')
@@ -31,6 +61,7 @@ def process_words():
     )
 
     return response
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
